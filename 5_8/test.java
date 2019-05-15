e639d2bf8f1a05daeafa74a9d6c264b9d805460d
djw326542578 <48444570+djw326542578@users.noreply.github.com>
2019-05-15 08:33:35 +0800

    test

---
+++ 5_8/test.java
@@ -0,0 +1,18 @@
+public class test{
+	/**
+	int field;
+	static int staticField;
+
+	public static void metbod (int param1, int pararm2) {
+		int variable1;
+		int variable2;
+	}*/
+	
+public static void main（String[ ] args）{
+	String a="中国";
+	String b="\0x4e2d\0x56fd";
+	
+	System.out.println(a);
+	System.out.println(b);
+	}
+}
